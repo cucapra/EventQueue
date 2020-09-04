a5dcd49ea92f23bdf7fff5e96f41d955a201392b
tissue3 <173666635@qq.com>
2020-09-03 22:13:22 -0400

    update to v12

--- include/EQueue/EQueueOps.h
+++ include/EQueue/EQueueOps.h
@@ -12,6 +12,7 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Function.h"
